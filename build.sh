a0c573686684f3a1d83d360072db00f1d9e4a589
khs1994 <khs1994@khs1994.com>
2018-03-10 14:08:12 +0800

    5.6.0

---
+++ build.sh
@@ -0,0 +1,120 @@
+#！/bin/bash
+
+set -e
+
+# 1. download
+
+if ! [ -d /usr/local/src/php-5.6.0 ];then
+
+cd /usr/local/src
+
+wget http://cn2.php.net/distributions/php-5.6.0.tar.gz
+
+tar -zxvf php-5.6.0.tar.gz
+
+fi
+
+cd /usr/local/src/php-5.6.0
+
+# 2. install packages
+
+sudo apt install -y autoconf \
+                   dpkg-dev \
+                   file \
+                   libc6-dev \
+                   make \
+                   pkg-config \
+                   re2c \
+                   gcc g++ \
+                   libedit-dev \
+                   zlib1g-dev \
+                   libxml2-dev \
+                   libssl-dev \
+                   libsqlite3-dev \
+                   libxslt1-dev \
+                   libcurl4-openssl-dev \
+                   libpq-dev \
+                   libmemcached-dev \
+                   libsasl2-dev \
+                   libfreetype6-dev \
+                   libpng-dev \
+                   libjpeg-dev
+
+# 3. bug
+
+debMultiarch="$(dpkg-architecture --query DEB_BUILD_MULTIARCH)"
+# https://bugs.php.net/bug.php?id=74125
+if [ ! -d /usr/include/curl ]; then
+    ln -sT "/usr/include/$debMultiarch/curl" /usr/local/include/curl
+fi
+
+# 4. configure
+
+./configure --prefix=/usr/local/php56 \
+    --with-config-file-path=/usr/local/php56/etc \
+    --with-config-file-scan-dir=/usr/local/php56/etc/conf.d \
+    --disable-cgi \
+    --enable-fpm \
+    --with-fpm-user=nginx \
+    --with-fpm-group=nginx \
+    --with-curl \
+    --with-gd \
+    --with-gettext \
+    --with-iconv-dir \
+    --with-kerberos \
+    --with-libedit \
+    --with-openssl \
+    --with-pcre-regex \
+    --with-pdo-mysql \
+    --with-pdo-pgsql \
+    --with-xsl \
+    --with-zlib \
+    --with-mhash \
+    --with-png-dir=/usr/lib \
+    --with-jpeg-dir=/usr/lib\
+    --with-freetype-dir=/usr/lib \
+    --enable-ftp \
+    --enable-mysqlnd \
+    --enable-bcmath \
+    --enable-libxml \
+    --enable-inline-optimization \
+    --enable-gd-jis-conv \
+    --enable-mbregex \
+    --enable-mbstring \
+    --enable-opcache \
+    --enable-pcntl \
+    --enable-shmop \
+    --enable-soap \
+    --enable-sockets \
+    --enable-sysvsem \
+    --enable-xml \
+    --enable-zip \
+    --enable-calendar \
+    --enable-intl \
+    --enable-exif
+
+# 5. make
+
+make
+
+# 6. make install
+
+sudo make install
+
+# 7. install extension
+
+export PHP_5_6_X=/usr/local/php56
+
+${PHP_5_6_X}/bin/php -v
+${PHP_5_6_X}/bin/pear config-set php_ini /usr/local/php56/etc/php.ini
+${PHP_5_6_X}/bin/pecl update-channels
+${PHP_5_6_X}/bin/pecl config-show
+${PHP_5_6_X}/bin/pecl install redis \
+                              memcached-2.2.0 \
+                              xdebug-2.5.5 \
+                              mongodb \
+                              yaml \
+                              igbinary
+# 8. enable extension
+
+# echo "zend_extension=opcache" > /usr/local/php/etc/conf.d/extension.ini
