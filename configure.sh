9a997dce735aed028bc8e0bbc7f09f5eb00a5289
khs1994 <khs1994@khs1994.com>
2025-12-07 12:00:46 +0800

    Sync from khs1994-php/configure@f1e052099b13fa10be93c7dea705093fcfc22fcf by PCIT

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
-PHP-master
+PHP-8.5.0
 
-`configure' configures PHP 8.6.0-dev to adapt to many kinds of systems.
+`configure' configures PHP 8.5.0 to adapt to many kinds of systems.
 
 Usage: ./configure [OPTION]... [VAR=VALUE]...
 
